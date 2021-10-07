188b991fb7e8457ec6f650a933cea106cb81c8d0
avi112bala <66358426+avi112bala@users.noreply.github.com>
2021-10-07 11:42:24 +0530

    Add files via upload

---
+++ fibbonacci.cpp
@@ -0,0 +1,13 @@
+
+void printTable(int start, int end, int step) {
+    
+
+    while(start<=end){
+        
+             int f=(start-32)*5.0/9;
+            cout<<start<<" "<<f<<endl;
+            start=start+step;
+    }
+        
+ 
+}
